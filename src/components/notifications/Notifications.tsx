265e8491fb50477770108bed3c0859a9595deefd
mariangelkruiz <marianruizdev@gmail.com>
2023-08-07 18:57:02 -0300

    new component

---
+++ src/components/notifications/Notifications.tsx
@@ -0,0 +1,20 @@
+import { useContext, useEffect, useState } from 'react';
+import Cookie from 'js-cookie';
+
+
+
+interface Props {
+  // Define las propiedades aquí
+}
+
+// Define el componente funcional
+const Notifications: React.FC<Props> = (props) => {
+  // Lógica y renderizado del componente
+  return (
+    <div>
+      {/* Contenido del componente */}
+    </div>
+  );
+};
+
+export default Notifications;
