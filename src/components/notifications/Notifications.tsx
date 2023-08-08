6c90457f18c91dfaf1077eeb97bd329cb795bcfd
mariangelkruiz <marianruizdev@gmail.com>
2023-08-07 22:48:05 -0300

    update

--- src/components/notifications/Notifications.tsx
+++ src/components/notifications/Notifications.tsx
@@ -4,15 +4,15 @@ import Cookie from 'js-cookie';
 
 
 interface Props {
-  // Define las propiedades aquí
+
 }
 
-// Define el componente funcional
+
 const Notifications: React.FC<Props> = (props) => {
   // Lógica y renderizado del componente
   return (
     <div>
-      {/* Contenido del componente */}
+      {}
     </div>
   );
 };
